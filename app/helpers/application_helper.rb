f8df82cf1ecb281ad514f6b4c11f05ed649a4eac
ylin10 <ylin10@bu.edu>
2016-12-16 11:04:06 -0500

    Using own full_title method to replace a manual insertion into every page.
    Need to review full_title method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
+  module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on RAILZ Tutorial Sample App"
+    if page_title.empty?
+      return base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
