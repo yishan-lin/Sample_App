4c1697dd9e6d8a7607623869b9ec3d1ae52fc8ea
ylin10 <ylin10@bu.edu>
2016-12-20 12:35:41 -0800

    Playing around with more methods.
    Ruby console work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,31 @@
   module ApplicationHelper
 
-  def full_title(page_title = '')
-    base_title = "Ruby on RAILZ Tutorial Sample App"
-    if page_title.empty?
-      return base_title
-    else
-      page_title + " | " + base_title
+    def full_title(page_title = '')
+      base_title = "Ruby on RAILZ Tutorial Sample App"
+      if page_title.empty?
+        return base_title
+      else
+        return page_title + " | " + base_title
+      end
     end
   end
-end
+
+
+  #def full_title(page_title = '')
+  #  base_title = "Ruby on RAILZ Tutorial Sample App"
+  #  if page_title.empty?
+  #    return base_title
+  #  else
+  #    return page_title + " | " + base_title
+  #  end
+  #end
+
+#end
+
+  #def full_title(page_title = '')
+  #  base_title = "Ruby on RAILZ Tutorial Sample App"
+  #  if page_title.empty?
+  #    return base_title
+  #  else
+  #    page_title + " | " + base_title
+  #  end
