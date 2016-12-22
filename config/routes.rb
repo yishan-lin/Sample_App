93184fccddcd67b404c414ad2a50f8e184ca15e4
ylin10 <ylin10@bu.edu>
2016-12-21 16:53:33 -0800

    Shortening of routes/condensing
    Instead of having to do long, winded “get ‘static_pages/help’, we
    condense it to just /help, and then tell RoR to update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/home'
-  get 'static_pages/help'
+
+  get 'help', to: 'static_pages#help' 
+  #get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/team'
