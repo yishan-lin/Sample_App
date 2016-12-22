2559497605d0b5faeb02eb9e214501c9a9698623
ylin10 <ylin10@bu.edu>
2016-12-22 15:26:22 -0800

    Adding validations to user parameters for DB

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "Example User", email: "example@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "email present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "name length maximum" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
