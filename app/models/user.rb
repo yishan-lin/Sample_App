0b24287e6c5e587e5c7bd98cbb5834d2be7ed942
ylin10 <ylin10@bu.edu>
2016-12-22 20:45:06 -0800

    Improving validation and testing via rails console, by .valid?
    Not the most easy to understand, but am going through this section a
    little faster because it’s more syntactically-challenging than
    conceptual understanding.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
 
-  validates :name, presence:true, length: { maximum: 50}
-  validates :email, presence:true, length: ( maximum: 255 )
+  before_save {self.email = email.downcase}
+
+  validates :name, presence:true, length: { maximum: 50}, uniqueness: {case_sensitive: false}
+
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence:true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  get root_path
-  assert_template 'static_pages/home'
-  assert_select "a[href=?]", about_path
-  assert_select "a[href=?]", help_path
-  assert_select "a[href=?]", team_path
   # test "the truth" do
   #   assert true
   # end
