a2b7b212334457a56d8e4a469487097259ce2e12
ylin10 <ylin10@bu.edu>
2016-12-25 20:31:07 -0800

    Manipulating minimum length of characters
    Change the minimum requirement for validations for users.rb in the
    models db

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :password, presence:true, length: {minimum: 6}
+  validates :password, presence:true, length: {minimum: 5}
 
 end
